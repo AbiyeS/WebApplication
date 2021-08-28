7464a761238e0950803ba7225455dedaa32bc945
AbiyeS <89671569+AbiyeS@users.noreply.github.com>
2021-08-28 02:23:12 +0100

    WebAPp

---
+++ static/js/main.js
@@ -0,0 +1,19 @@
+<script type="text/javascript">
+
+var images = new Array();
+
+function preloadImages(){
+
+    for (i=0; i < preloadImages.arguments.length; i++){
+
+         images[i] = new Image();
+
+        images[i].src = preloadImages.arguments[i];
+
+    }
+
+}
+
+preloadImages("icon-logo.jpg", "icon-power.png", "icon-profile.png");
+
+</script>
